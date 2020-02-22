7d4cf4eb47b8d043b2ffd97f726c95fff876f9ee
DS37676X <dedysimamora627@gmail.com>
2020-02-23 02:51:50 +0700

    number up count done

--- src/Components/DocumentCard.js
+++ src/Components/DocumentCard.js
@@ -1,50 +1,47 @@
 import React, {useState} from 'react'
 import { Card} from 'antd'
-import CountUp from 'react-countup';
-import { ResponsiveContainer, PieChart, Pie, Sector, Cell } from 'recharts';
+import { useCountUp } from 'react-countup';
+import {PieChart, Pie, Sector, Cell } from 'recharts';
   
 import "./DocumentCard.css"
 
+
+const CountNumber = (props) => {
+  const { countUp } = useCountUp({ end: props.endNumber, suffix: props.suffix });
+  return countUp
+};
+
 const DocumentCard = (props) => {
-
-    const [animation, setAnimation] = useState(false)
-
-    const endAnimation = (props) => {
-        console.log(props, "<<<<<<<<<<<<<");
-        setAnimation(true)
-        
-    }
 
     const data = [
         { name: 'Dokumen', value: 100 },
         { name: 'Foto', value: 300 },
         { name: 'Video', value: 200 }
     ]
-
     data.sort(function(x,y){ return x.name == props.sortData ? -1 : y.name == props.sortData ? 1 : 0; });
-    
-
     const Color = ['#EE682A', '#291B4F', '#FCD42B'];
-
     Color.sort(function(x,y){ return x == props.sortColor ? -1 : y == props.sortColor ? 1 : 0; });
 
+
     const renderActiveShape = (props) => {
-        
-        const RADIAN = Math.PI / 180;
-        const {
-          cx, cy, midAngle, innerRadius, outerRadius, startAngle, endAngle,
-          fill, payload, percent, value,
-        } = props;
-        const sin = Math.sin(-RADIAN * midAngle);
-        const cos = Math.cos(-RADIAN * midAngle);
-        const sx = cx + (outerRadius + 10) * cos;
-        const sy = cy + (outerRadius + 10) * sin;
-        const mx = cx + (outerRadius + 30) * cos;
-        const my = cy + (outerRadius + 30) * sin;
-        const ex = mx + (cos >= 0 ? 1 : -1) * 22;
-        const ey = my;
-        const textAnchor = cos >= 0 ? 'start' : 'end';
-        
+      const RADIAN = Math.PI / 180;
+      const {
+        cx, cy, midAngle, innerRadius, outerRadius, startAngle, endAngle,
+        fill, payload, percent, value,
+      } = props;
+      const sin = Math.sin(-RADIAN * midAngle);
+      const cos = Math.cos(-RADIAN * midAngle);
+      const sx = cx + (outerRadius + 10) * cos;
+      const sy = cy + (outerRadius + 10) * sin;
+      const mx = cx + (outerRadius + 30) * cos;
+      const my = cy + (outerRadius + 30) * sin;
+      const ex = mx + (cos >= 0 ? 1 : -1) * 22;
+      const ey = my;
+      const textAnchor = cos >= 0 ? 'start' : 'end';
+      let Percentage = <CountNumber endNumber={(percent * 100).toFixed(2)} suffix=" %" />
+      let TotalArsip = <CountNumber endNumber={value} suffix=" Arsip" />
+      
+      
       
         return  (
           <g>
@@ -58,12 +55,10 @@ const DocumentCard = (props) => {
               endAngle={endAngle}
               fill={fill}
             />
-
-            
-
-            <text style={{fontSize:'40px', fontWeight:700}} width={70} x={innerRadius + 135} y={innerRadius + 40} textAnchor={textAnchor} fill={fill}>{`${(percent * 100).toFixed(2)}%`}</text>
-            <text x={innerRadius + 140} y={innerRadius + 60} textAnchor={textAnchor} fill={fill}>{`Total ${payload.name} ${value}`}</text>
-            
+            <text style={{fontSize:'12px', fontWeight:500}} x={cx} y={cy} dy={8} textAnchor="middle" fill={fill}>{payload.name}</text>
+            <text style={{fontSize:'40px', fontWeight:700}} width={70} x={innerRadius + 135} y={innerRadius + 40} textAnchor={textAnchor} fill={fill}>{Percentage}</text>
+            <text x={innerRadius + 140} y={innerRadius + 60} textAnchor={textAnchor} fill={fill}>Total</text>
+            <text x={innerRadius + 173} y={innerRadius + 60} textAnchor={textAnchor} fill={fill}>{TotalArsip}</text>
           </g>
         );
       };
@@ -82,7 +77,6 @@ const DocumentCard = (props) => {
                         fill="#8884d8"
                         paddingAngle={5}
                         dataKey="value"
-                        onAnimationEnd={endAnimation}
                         >
                         {
                             data.map((entry, index) => <Cell key={`cell-${index}`} fill={Color[index % Color.length]} />)
--- src/Containers/Dashboard.js
+++ src/Containers/Dashboard.js
@@ -1,10 +1,31 @@
 import React from 'react'
 import DocumentCard from "../Components/DocumentCard"
 import { Col, Row } from 'antd'
+import {
+    BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend,
+  } from 'recharts';
 
 const Dashboard = () => {
+
+
+    const data = [
+        {
+          name: 'Dokumen', uv: 4000, amt: 2400,
+        },
+        {
+          name: 'Foto', uv: 3000, amt: 2210,
+        },
+        {
+          name: 'Video', uv: 2000, amt: 2290,
+        }
+      ];
+
+
+
+
     return (
-        <Row gutter={16}>
+        <div>
+            <Row gutter={16}>
             <Col  xs={24} sm={24} md={8} lg={8}>
                 <DocumentCard sortData={"Dokumen"} sortColor={'#EE682A'} />
             </Col>
@@ -15,6 +36,26 @@ const Dashboard = () => {
                 <DocumentCard sortData={"Video"} sortColor={'#FCD42B'} />
             </Col>
         </Row>
+        <Row>
+            <Col span={24}>
+
+            <BarChart
+                width={500}
+                height={350}
+                data={data}
+                margin={{
+                top: 25, right: 30, left: 20, bottom: 5,
+                }}
+            >
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey="name" />
+                <YAxis />
+                <Bar dataKey="uv" fill="#82ca9d" />
+            </BarChart>
+                
+            </Col>
+        </Row>
+        </div>
     )
 }
 
