872e0599250ee893806102001b6577cb91cfaf0f
Dedy Simamora <DS37676X@cimbniaga.co.id>
2021-03-25 12:21:50 +0700

    edit siakum and allow dot in number input

--- src/Components/AtomComponent/Input/NumberInput.js
+++ src/Components/AtomComponent/Input/NumberInput.js
@@ -1,39 +1,51 @@
-import React from 'react'
-import { Form, Input} from 'antd';
+import React from "react";
+import { Form, Input } from "antd";
 import Cleave from "cleave.js/dist/cleave-react-node";
-import "./NumberInput.css"
+import "./NumberInput.css";
 
 const NumberInput = (props) => {
-    
-    const { id, name, placeholder, error, helperText, maxLength, defaultValue, onChange, onFocus, customClassName, width} = props;
-    return (
-        <Form.Item
-        validateStatus={error ? "error" : "validating"}
-        help={helperText.length !== 0 ? helperText : ""}
-        style={{ textAlign: "left", width: width }}
-        >
-            <Input.Group >
-                <Cleave
-                    id={name}
-                    name={name}
-                    value={defaultValue}
-                    maxLength={maxLength}
-                    placeholder={placeholder}
-                    onChange={event => {
-                        const tempEvent = event;
-                        tempEvent.target.value = event.target.rawValue;
-                        onChange(tempEvent);
-                    }}
-                    onFocus={onFocus}
-                    className={`ant-input input-number ${customClassName}`}
-                    style={{paddingRight:'0px'}}
-                    options={{
-                        numericOnly: true
-                    }}
-                />
-                </Input.Group>
-        </Form.Item>
-    )
-}
+  const {
+    id,
+    name,
+    placeholder,
+    error,
+    helperText,
+    maxLength,
+    defaultValue,
+    onChange,
+    onFocus,
+    customClassName,
+    width,
+  } = props;
+  return (
+    <Form.Item
+      validateStatus={error ? "error" : "validating"}
+      help={helperText.length !== 0 ? helperText : ""}
+      style={{ textAlign: "left", width: width }}
+    >
+      <Input.Group>
+        <Cleave
+          id={name}
+          name={name}
+          value={defaultValue}
+          maxLength={maxLength}
+          placeholder={placeholder}
+          onChange={(event) => {
+            const tempEvent = event;
+            tempEvent.target.value = event.target.rawValue;
+            onChange(tempEvent);
+          }}
+          onFocus={onFocus}
+          className={`ant-input input-number ${customClassName}`}
+          style={{ paddingRight: "0px" }}
+          options={{
+            numeral: true,
+            delimiter: ".",
+          }}
+        />
+      </Input.Group>
+    </Form.Item>
+  );
+};
 
-export default NumberInput
+export default NumberInput;
